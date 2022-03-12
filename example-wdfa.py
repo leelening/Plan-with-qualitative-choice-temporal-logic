64c132d8d314f15b806048fc820aaffafaf7df5b
lening <lli4@wpi.edu>
2022-03-12 11:17:39 -0500

    working

--- example-wdfa.py
+++ example-wdfa.py
@@ -1,10 +1,16 @@
-# illustrative examples
+__author__ = "Jie Fu and Lening Li"
+__email__ = "fujie@ufl.edu and lli4@wpi.edu"
+__version__ = "0.0.1"
+__maintainer__ = "Jie Fu and Lening Li"
+__description__ = (
+    "This code provides illustrative examples for creating wdfa and some defined "
+    "operations including: orderedOR, prioritized_conj, and prioritized_disj"
+)
 
-from automata.fa.dfa import DFA
 from WDFA import *
 
 # Eventually A
-dfa = DFA(
+dfa1 = DFA(
     states={"0", "1"},
     input_symbols={"a", "b", "E"},
     transitions={
@@ -14,6 +20,7 @@ dfa = DFA(
     initial_state="0",
     final_states={"1"},
 )
+dfa1.validate()
 
 
 # eventually B
@@ -27,6 +34,7 @@ dfa2 = DFA(
     initial_state="0",
     final_states={"1"},
 )
+dfa2.validate()
 
 # not A until B
 dfa3 = DFA(
@@ -40,16 +48,33 @@ dfa3 = DFA(
     initial_state="0",
     final_states={"1"},
 )
-wdfa1 = get_wdfa_from_dfa(dfa)
-wdfa2 = get_wdfa_from_dfa(dfa2)
-wdfa3 = get_wdfa_from_dfa(dfa3)
-orderedDFA12 = orderedOR(dfa, dfa2)
+dfa3.validate()
+
+# try ordered OR between dfa1 and dfa2
+orderedDFA12 = orderedOR(dfa1, dfa2)
+orderedDFA12.show_diagram(path="./orderedDFA12.png")
+
+# try ordered OR between dfa3 and dfa2
 orderedDFA = orderedOR(dfa3, dfa2)
-orderedDFA2 = generalized_orderedOR(orderedDFA, dfa)
+orderedDFA.show_diagram(path="./orderedDFA.png")
+
+# try generalized ordered OR between
+orderedDFA2 = generalized_orderedOR(orderedDFA, dfa1)
+orderedDFA2.show_diagram(path="./orderedDFA2.png")
+
+# create 3 wdfa
+wdfa1 = get_wdfa_from_dfa(dfa1)
+wdfa1.validate()
+wdfa2 = get_wdfa_from_dfa(dfa2)
+wdfa2.validate()
+wdfa3 = get_wdfa_from_dfa(dfa3)
+wdfa3.validate()
+
+# try the prioritized conjunction between wdfa3 and wdfa2
 conj_wdfa = prioritized_conj(wdfa3, wdfa2)
 conj_wdfa.trim()
-orderedDFA.show_diagram(path="./orderedDFA.png")
-orderedDFA2.show_diagram(path="./orderedDFA2.png")
 conj_wdfa.show_diagram(path="./conj_wdfa.png")
+
+# try the prioritized disconjunction between wdfa3 and wdfa2
 disj_wdfa = prioritized_disj(wdfa3, wdfa2)
 disj_wdfa.show_diagram(path="./disj_wdfa.png")
