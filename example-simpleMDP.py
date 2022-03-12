7c767fd3ab7ee86fbf27ebf43d1a581757321393
lening <lli4@wpi.edu>
2022-03-12 13:13:12 -0500

    not working

--- example-simpleMDP.py
+++ example-simpleMDP.py
@@ -1,18 +1,14 @@
-from MDP import *
+from MDP import MDP as M
 from ProductMDP import *
 
-mdp = read_from_file_MDP("ex.txt")
-mdp.init = 0
-for s in mdp.states:
-    mdp.labeling(s, "E")  # 'E' is for neither a or b.
-mdp.labeling(3, "a")
-mdp.labeling(6, "b")
-Labels = ["a", "b", "E"]
+mdp = M(file_path="ex.yaml")
+mdp.validate()
+print(mdp)
 
-exec(open("./example-wdfa.py").read())
-# the objective is to reach a state labeled 'b'.
-productmdp, reward = product_mdp(mdp, orderedDFA12)
-from Policy import *
-
-Vstate1, policyT = value_iter(productmdp, reward)
-print("end")
+# exec(open("./example-wdfa.py").read())
+# # the objective is to reach a state labeled 'b'.
+# productmdp, reward = product_mdp(mdp, orderedDFA12)
+# from Policy import *
+#
+# Vstate1, policyT = value_iter(productmdp, reward)
+# print("end")
