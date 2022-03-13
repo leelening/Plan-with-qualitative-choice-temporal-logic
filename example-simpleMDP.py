18879d730b7db8f54d76a454c04fe5a57997ba2a
lening <lli4@wpi.edu>
2022-03-12 21:30:55 -0500

    seems working

--- example-simpleMDP.py
+++ example-simpleMDP.py
@@ -1,27 +1,32 @@
+from torch._jit_internal import ignore
+
 from mdp import MDP
 from product_mdp import ProductMDP
+import os
+import pandas as pd
 
-print("Initialize the MPD ...")
+print("Initializing the MPD ...")
 mdp = MDP(file_path="ex.yaml")
-print("Validate the MDP ...")
+print("Validating the MDP ...")
 mdp.validate()
 print("Finish validation of MDP! Printing the detailed information ...")
 print(mdp)
 print("Finish printing the MDP ...")
-print("Saving the MDP info into ./")
+print("Saving the MDP info into ./MDP.csv")
 mdp.save()
 
-print("Initialize the WDFA ...")
+print("Initializing the WDFA ...")
 exec(open("./example-wdfa.py").read())
-print("Construct the product MDP ...")
+print("Constructing the product MDP ...")
 # the objective is to reach a state labeled 'b'.
+automaton = "orderedDFA12"
 product_mdp = ProductMDP(mdp, orderedDFA12)
-print("Validate the product MDP ...")
+print("Validating the product MDP ...")
 # explicitly validate the mdp
 product_mdp.validate()
 print("Finish validation of product MDP! Printing the detailed information ...")
 print(product_mdp)
-print("Saving the Product MDP info into ./")
+print("Saving the Product MDP info into ./ProductMDP.csv")
 product_mdp.save()
 
 from lp_mdp import LP
@@ -29,11 +34,12 @@ from lp_mdp import LP
 print("Start computing the product MDP ...")
 df = LP(product_mdp)
 print("End of the computation of the product MDP...")
-print("Save results of product MDP ...")
+print("Saving results of product MDP ...")
 df.to_csv("./results.csv")
 
 print("Start simulating the product MDP ...")
 state = product_mdp.init
+
 
 counter = 0
 trajectory = []
@@ -47,4 +53,20 @@ while counter <= 100 and state != "v_sink":
     counter += 1
 
 print("Finish simulating the product MDP ...")
+print("Printing trajectory ...")
 print("Trajectory: {}".format([x[-1][0] for x in trajectory]))
+print("Saving trajectory ...")
+if os.path.exists("trajectory.csv"):
+    df1 = pd.read_csv("trajectory.csv")
+else:
+    df1 = pd.DataFrame({"Description": [], "Trajectory": []})
+df1 = df1.append(
+    {
+        "Description": "mdp_{}_automaton_{}".format(mdp.name, str(automaton)),
+        "Trajectory": trajectory,
+        "States": [x[-1][0] for x in trajectory],
+    },
+    ignore_index=True,
+)
+df1.to_csv("trajectory.csv", index=False)
+print("End")
