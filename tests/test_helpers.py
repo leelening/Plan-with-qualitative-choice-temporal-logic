b5c72787093961388637a329d077da9d03f8f433
lening <lli4@wpi.edu>
2022-06-18 18:47:21 -0400

    update

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,4 +1,26 @@
+import pytest
 from wdfa.helpers import sync
+from wdfa.helpers import ordered_or, prioritized_conj
+
+ORDERED_OR_TEST_CASE = (
+    (("0", "0"), "E", ("0", "0"), 0),
+    (
+        ("0", "0"),
+        "a",
+        ("1", "0"),
+        0,
+    ),
+    (("1", "1"), "end", "sink", 1),
+    (("0", "1"), "end", "sink", 2),
+    (("1", "0"), "end", "sink", 1),
+    ("sink", "end", "sink", 0),
+    ("sink", "a", "sink", 0),
+)
+
+PRIORITIZED_CONJ_TEST_CASE = (
+    (("1", "1"), "end", "sink", 1),
+    (("2", "1"), "end", "sink", 0),
+)
 
 
 def test_set_options(get_wdfa_from_eventually_a_dfa):
@@ -12,3 +34,22 @@ def test_sync(get_wdfa_from_eventually_a_dfa, get_wdfa_from_eventually_b_dfa):
     sync_wdfa = sync(get_wdfa_from_eventually_a_dfa, get_wdfa_from_eventually_b_dfa)
     print(sync_wdfa)
     sync_wdfa.validate()
+
+
+@pytest.mark.parametrize("q, a, nq, w", ORDERED_OR_TEST_CASE)
+def test_ordered_or(
+    get_wdfa_from_eventually_a_dfa, get_wdfa_from_eventually_b_dfa, q, a, nq, w
+):
+    wdfa = ordered_or(get_wdfa_from_eventually_a_dfa, get_wdfa_from_eventually_b_dfa)
+    print(get_wdfa_from_eventually_a_dfa)
+    assert wdfa.weight[q, a, nq] == w
+
+
+@pytest.mark.parametrize("q, a, nq, w", PRIORITIZED_CONJ_TEST_CASE)
+def test_prioritized_conj(
+    get_wdfa_from_not_a_until_b_dfa, get_wdfa_from_eventually_a_dfa, q, a, nq, w
+):
+    wdfa = prioritized_conj(
+        get_wdfa_from_not_a_until_b_dfa, get_wdfa_from_eventually_a_dfa
+    )
+    assert wdfa.weight[q, a, nq] == w
--- tests/test_wdfa.py
+++ tests/test_wdfa.py
@@ -1,8 +1,8 @@
 import pytest
 from automata.fa.dfa import DFA
 from itertools import product
+from wdfa.helpers import get_wdfa_from_dfa
 
-from wdfa.helpers import get_wdfa_from_dfa, ordered_or, prioritized_conj
 
 dfa = DFA(
     states={"0", "1"},
@@ -36,42 +36,3 @@ def test_completeness():
         assert wdfa.transitions[q][a] in wdfa.states
 
 
-ORDERED_OR_TEST_CASE = (
-    (("0", "0"), "E", ("0", "0"), 0),
-    (
-        ("0", "0"),
-        "a",
-        ("1", "0"),
-        0,
-    ),
-    (("1", "1"), "end", "sink", 1),
-    (("0", "1"), "end", "sink", 2),
-    (("1", "0"), "end", "sink", 1),
-    ("sink", "end", "sink", 0),
-    ("sink", "a", "sink", 0),
-)
-
-
-@pytest.mark.parametrize("q, a, nq, w", ORDERED_OR_TEST_CASE)
-def test_ordered_or(
-    get_wdfa_from_eventually_a_dfa, get_wdfa_from_eventually_b_dfa, q, a, nq, w
-):
-    wdfa = ordered_or(get_wdfa_from_eventually_a_dfa, get_wdfa_from_eventually_b_dfa)
-    print(get_wdfa_from_eventually_a_dfa)
-    assert wdfa.weight[q, a, nq] == w
-
-
-PRIORITIZED_CONJ_TEST_CASE = (
-    (("1", "1"), "end", "sink", 1),
-    (("2", "1"), "end", "sink", 0),
-)
-
-
-@pytest.mark.parametrize("q, a, nq, w", PRIORITIZED_CONJ_TEST_CASE)
-def test_prioritized_conj(
-    get_wdfa_from_not_a_until_b_dfa, get_wdfa_from_eventually_a_dfa, q, a, nq, w
-):
-    wdfa = prioritized_conj(
-        get_wdfa_from_not_a_until_b_dfa, get_wdfa_from_eventually_a_dfa
-    )
-    assert wdfa.weight[q, a, nq] == w
