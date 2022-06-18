f2abcc5f9e672c73cf261eb986b22f85b27e8e30
lening <lli4@wpi.edu>
2022-06-18 18:06:19 -0400

    update

--- tests/test_product_mdp.py
+++ tests/test_product_mdp.py
@@ -1,7 +1,5 @@
 from numpy.testing import assert_array_equal
 import numpy as np
-from itertools import product
-from tabulate import tabulate
 
 
 def test_product_mdp_transition(construct_product_mdp):
